6733cca820b3934a6f57ce5bf444be4897043da8
Harshad Hegde <hegdehb@gmail.com>
2024-07-05 14:56:52 -0500

    Added input schema

--- src/microbe_traits_ingest/__init__.py
+++ src/microbe_traits_ingest/__init__.py
@@ -1,8 +1,29 @@
 """microbe-traits-ingest package."""
 import importlib_metadata
+from pathlib import Path
+from linkml_runtime.utils.schemaview import SchemaView
+from linkml.generators.pythongen import PythonGenerator
 
 try:
     __version__ = importlib_metadata.version(__name__)
 except importlib_metadata.PackageNotFoundError:
     # package is not installed
     __version__ = "0.0.0"  # pragma: no cover
+
+
+# Define the path to the current file's directory
+PWD = Path(__file__).parent
+SCHEMA_DIR = PWD / "schema"
+SCHEMA_YAML = SCHEMA_DIR / "traits.yaml"
+SCHEMA_PYTHON = SCHEMA_DIR / "traits.py"
+
+schema_view = SchemaView(SCHEMA_YAML)
+
+# Generate the Python classes from the schema
+python_generator = PythonGenerator(schema_view.schema)
+python_code = python_generator.serialize()
+
+
+# Write the generated Python code to the traits.py file within the kg_traits directory
+with open(SCHEMA_PYTHON, "w") as f:
+    f.write(python_code)
